782650396bd92b09f6b3911999238e277bba4a7f
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-07-27 18:14:11 -0400

    Agrendo navbar en el header y contenido en el footer

---
+++ templates/footer.php
@@ -0,0 +1,85 @@
+
+        <footer class="bg-dark text-white pt-5 pb-4">
+            <!--https://www.youtube.com/watch?v=PwEvMY7nnY8&ab_channel=ADesignerWhoCodes-->
+            <div class="container text-center text-md-start">
+                <div class="row text-center text-md-start">
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Nosotros</h5>
+                        <hr class="mb-4">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officiis facilis molestias impedit est, mollitia iste accusamus deserunt explicabo natus recusandae, tempora sunt veritatis quia nemo. Quas eaque rem eos.</p>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Dejanos ayudarte</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <a href="#" class="text-white">Tu cuenta</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white">Tu Ordenes</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white">Manejo de libros</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white">Ayuda</a>
+                    </p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Has dinero</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <a href="#" class="text-white">Vende productos en nuestro sitio</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white">Anuncios</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white">Afiliate</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-white">Publicar</a>
+                    </p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Contacto</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <li class="fas fa-home me-3"></li>Rep. Dom.
+                    </p>
+                    <p>
+                        <li class="fas fa-envelope me-3"></li>correo@correo.com
+                    </p>
+                    <p>
+                        <li class="fas fa-phone me-3"></li>correo@correo.com
+                    </p>
+                    <p>
+                        <li class="fas fa-print me-3"></li>correo@correo.com
+                    </p>
+                </div>
+                <hr class="mb-4">
+                <div class="text-center mb2">
+                    <p>
+                        &copy; Todos los derechos reservados
+                        <a href="#">
+                            <strong class="text-primary">Enmanuel</strong>
+                        </a>
+                    </p>
+                </div>
+                <div class="text-center">
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item"><a href="" class="text-white">Facebook</a></li>
+                        <li class="list-inline-item"><a href="" class="text-white">Facebook</a></li>
+                        <li class="list-inline-item"><a href="" class="text-white">Facebook</a></li>
+                    </ul>
+                </div>
+                </div>
+
+                
+            </div>
+        </footer>
+        <script src="http://localhost/Proyecto-final-web/assets/js/custom/main.js"></script>
+        <script src="http://localhost/Proyecto-final-web/assets/js/Bootstrap-js/bootstrap.bundle.js"></script>
+    </body>
+</html>
---
+++ templates/head.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/Proyecto-final-web/assets/css/custom/Main.css">
+    <link rel="stylesheet" href="http://localhost/Proyecto-final-web/assets/css/Bootstrap-css/bootstrap.css">
+    
+    <title>Document</title>
+</head>
+
+<body>
+<header class="body-header">
+
+<!-- Home of menu -->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+    <div class="container-fluid">
+
+        <!-- icon or name -->
+        <a class="navbar-brand" href="http://localhost/Proyecto-final-web/index.php">
+            <i class="bi bi-flower1"></i>
+            <span class="text-warning">Librería</span>
+        </a>
+
+        <!-- button menu -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- collapsable menu elements -->
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="http://localhost/Proyecto-final-web/index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="http://localhost/Proyecto-final-web/views/books/books.php">Libros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="http://localhost/Proyecto-final-web/views/autors/autors.php">Autores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="http://localhost/Proyecto-final-web/views/contact/contact.php">Contacto</a>
+                </li>
+                <li class="nav-item dropdown" data-bs-theme="dark">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Practices
+                    </a>
+
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Practice 1</a></li>
+                        <!-- <li><a class="dropdown-item" href="#">Practice 2</a></li>
+                        <li><a class="dropdown-item" href="#">Practice 3</a></li>
+                        <li><a class="dropdown-item" href="#">Practice 4</a></li>
+                        <li><a class="dropdown-item" href="#">Practice 5</a></li>
+                        <li><a class="dropdown-item" href="#">Practice 6</a></li> -->
+                    </ul>
+                </li>
+            </ul>
+
+            <hr class="d-md-none text-white-50">
+            
+
+            <!-- Social media links -->
+            <p class="d-md-none text-light">Buscador</p>
+            <form class="d-flex" role="search">
+                    <input class="form-control me-2 my-1" type="search" placeholder="Buscar libro / autor..." aria-label="Search">
+                    <button class="btn btn-outline-success my-1" type="submit">Buscar</button>
+            </form>
+</header>
