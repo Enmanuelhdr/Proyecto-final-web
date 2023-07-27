e283cb8bb4a79ba7412d1df03cde14eb32327ab2
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-07-27 18:17:36 -0400

    Creando el home

---
+++ views/home/home.php
@@ -0,0 +1,66 @@
+<main>
+    <div class="main_slider">
+        <div class="container_carousel">
+            <div class="carruseles" id="slider">
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-01.jpeg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-02.jpg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-03.jpg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-04.jpg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-05.jpg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-06.jpg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-07.jpg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-08.jpg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-09.jpg" alt="">
+                </section>
+                <section class="slider_section">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-10.jpg" alt="">
+                </section>
+            </div>
+            <div class="btn_left"><</div>
+            <div class="btn_right">></div>
+        </div>
+    </div>
+    
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nam ex earum, quibusdam iste similique illum reiciendis mollitia? Accusamus consequatur aperiam enim voluptas, quo inventore nisi adipisci sit suscipit mollitia. Vero fuga consequatur temporibus explicabo, illum in! Dolorum, id minima.</p>
+    </section>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nam ex earum, quibusdam iste similique illum reiciendis mollitia? Accusamus consequatur aperiam enim voluptas, quo inventore nisi adipisci sit suscipit mollitia. Vero fuga consequatur temporibus explicabo, illum in! Dolorum, id minima.</p>
+    </section>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nam ex earum, quibusdam iste similique illum reiciendis mollitia? Accusamus consequatur aperiam enim voluptas, quo inventore nisi adipisci sit suscipit mollitia. Vero fuga consequatur temporibus explicabo, illum in! Dolorum, id minima.</p>
+    </section>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nam ex earum, quibusdam iste similique illum reiciendis mollitia? Accusamus consequatur aperiam enim voluptas, quo inventore nisi adipisci sit suscipit mollitia. Vero fuga consequatur temporibus explicabo, illum in! Dolorum, id minima.</p>
+    </section>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nam ex earum, quibusdam iste similique illum reiciendis mollitia? Accusamus consequatur aperiam enim voluptas, quo inventore nisi adipisci sit suscipit mollitia. Vero fuga consequatur temporibus explicabo, illum in! Dolorum, id minima.</p>
+    </section>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nam ex earum, quibusdam iste similique illum reiciendis mollitia? Accusamus consequatur aperiam enim voluptas, quo inventore nisi adipisci sit suscipit mollitia. Vero fuga consequatur temporibus explicabo, illum in! Dolorum, id minima.</p>
+    </section>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nam ex earum, quibusdam iste similique illum reiciendis mollitia? Accusamus consequatur aperiam enim voluptas, quo inventore nisi adipisci sit suscipit mollitia. Vero fuga consequatur temporibus explicabo, illum in! Dolorum, id minima.</p>
+    </section>
+
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nam ex earum, quibusdam iste similique illum reiciendis mollitia? Accusamus consequatur aperiam enim voluptas, quo inventore nisi adipisci sit suscipit mollitia. Vero fuga consequatur temporibus explicabo, illum in! Dolorum, id minima.</p>
+    </section>
+</main>
