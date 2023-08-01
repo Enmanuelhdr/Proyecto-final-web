2096d8e36bfcba688605d53f67d7054872d4920d
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-08-01 03:23:57 -0400

    Ingresando los registros de la base de datos a la vista libros

--- views/books/books.php
+++ views/books/books.php
@@ -1,5 +1,9 @@
 <?php
-    require_once("../../templates/head.php"); 
+    require_once("../../templates/head.php");
+    require_once("../../controllers/booksController.php");
+    $obj = new booksController();
+    $books = $obj -> showBooks();
+    /* print_r($obj -> showBooks()); */
 ?>
 
 <main>
@@ -42,53 +46,22 @@
     <hr class="mx-5 px-5">
     <h3 class="display-3 my-5 py-5 text-center">Listado de libros</h3>
     <div class="container mb-3">
-        <div class="row g-3">
+        <div class="row g-4">
+        <?php foreach($books as $book){?>
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="card">
                     <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-02.jpg" alt="" srcset="" class="card-img-top">
                     <div class="card-body">
-                        <h5 class="card-title">Libros</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
-                    </div>
-                </div>
-                
-            </div>
-            <div class="col-12 col-md-6 col-lg-4">
-            <div class="card">
-                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-03.jpg" alt="" srcset="" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Libros</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                        <h5 class="card-title"><?php echo $book['titulo']; ?></h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary"><?php echo ucfirst($book['tipo']); ?></h6>
+                        <p class="card-text"><?php echo $book['notas']; ?></p>
+                        <a href="#" class="btn btn-primary">Ver información</a>
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-4">
-            <div class="card">
-                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-04.jpg" alt="" srcset="" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Libros</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4">
-            <div class="card">
-                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-04.jpg" alt="" srcset="" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Libros</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4">
-            <div class="card">
-                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-04.jpg" alt="" srcset="" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">Libros</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
-                    </div>
-                </div>
-            </div>
+        <?php }?>    
+            
+        
         </div>
     </div>
 </main>
