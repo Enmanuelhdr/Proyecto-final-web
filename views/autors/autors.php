58ca2c54d4a0fce295c4fe59b49b321dc9e28cd3
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-07-31 23:35:58 -0400

    Agregando estructura de como se veran los datos

---
+++ views/autors/autors.php
@@ -0,0 +1,96 @@
+<?php
+    require_once("../../templates/head.php"); 
+?>
+<main>
+    <h2 class="display-1 mt-5 pt-5 text-center fw-normal">Catalogo de Autores</h2>
+    <hr class="mx-5 px-5">
+    <h3 class="display-3 my-5 py-5 text-center">Autores celebres</h3>
+    <div class="container mb-3">
+        <div class="row g-3">
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-02.jpg" alt="" srcset="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Libros</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-03.jpg" alt="" srcset="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Libros</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-04.jpg" alt="" srcset="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Libros</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <hr class="mx-5 px-5">
+    <h3 class="display-3 my-5 py-5 text-center">Listado de autores</h3>
+    <div class="container mb-3">
+        <div class="row g-3">
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-02.jpg" alt="" srcset="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Libros</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-03.jpg" alt="" srcset="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Libros</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-04.jpg" alt="" srcset="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Libros</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-04.jpg" alt="" srcset="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Libros</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+                    <img src="http://localhost/Proyecto-final-web/assets/img/home/home-img-slider-04.jpg" alt="" srcset="" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Libros</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, iusto ea dolore optio dolor maiores sequi ex animi, vel consequatur sed est earum magni quasi quisquam veritatis quo totam labore?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php
+    require_once("../../templates/footer.php");  
+?>
