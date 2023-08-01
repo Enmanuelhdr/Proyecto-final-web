027e3ef614abf46122f0a20d67cecc4b64b6c6c3
Enmanuelhdr <ehdelrosario49@gmail.com>
2023-08-01 03:22:10 -0400

    Estableciendo codificacion final de contactoModel

--- models/contactModel.php
+++ models/contactModel.php
@@ -16,22 +16,6 @@
             $stament->bindParam(":id",$id);
             return ($stament->execute()) ? $stament->fetch() : false ;
         }
-
-        /*public function index(){
-            $stament = $this->PDO->prepare("SELECT * FROM username");
-            return ($stament->execute()) ? $stament->fetchAll() : false;
-        }
-        public function update($id,$nombre){
-            $stament = $this->PDO->prepare("UPDATE username SET nombre = :nombre WHERE id = :id");
-            $stament->bindParam(":nombre",$nombre);
-            $stament->bindParam(":id",$id);
-            return ($stament->execute()) ? $id : false;
-        }
-        public function delete($id){
-            $stament = $this->PDO->prepare("DELETE FROM username WHERE id = :id");
-            $stament->bindParam(":id",$id);
-            return ($stament->execute()) ? true : false;
-        } */
     }
 
 ?>
